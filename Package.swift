e5faa5c17a77b5560db62ce8699123c23181b1fa
tyagishi <tyagishi@gmail.com>
2026-04-08 15:38:28 +0900

    initial

--- Package.swift
+++ Package.swift
@@ -5,18 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "SDSMarkdownClipPluginProtocol",
+    platforms: [
+        .macOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SDSMarkdownClipPluginProtocol",
+            type: .dynamic,
             targets: ["SDSMarkdownClipPluginProtocol"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/tyagishi/SDSMarkdownEditView", from: "2.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SDSMarkdownClipPluginProtocol"
+            name: "SDSMarkdownClipPluginProtocol",
+            dependencies: ["SDSMarkdownEditView"],
         ),
 
     ],
--- Sources/SDSMarkdownClipPluginProtocol/SDSMarkdownClipPluginProtocol.swift
+++ Sources/SDSMarkdownClipPluginProtocol/SDSMarkdownClipPluginProtocol.swift
@@ -1,2 +1,31 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import SDSMarkdownEditView
+import SwiftUI
+
+public protocol MarkdownClipGeneralProtocol {
+    init()
+    var name: String { get }
+}
+
+public protocol MarkdownClipTextPlugin: MarkdownClipGeneralProtocol {
+    func editText(text: String, at: String.Index) -> (newText: String, replace: Range<String.Index>, cursorAt: String.Index?)
+}
+
+public protocol MarkdownClipGUIPlugin: MarkdownClipGeneralProtocol {
+    var textViewModel: MarkdownTextViewModel? { get set }
+
+    @MainActor
+    @ViewBuilder
+    func sheet() -> AnyView
+}
+
+@objc
+open class PluginBuilder: NSObject {
+    public required override init() {}
+    open func plugins() -> [MarkdownClipGeneralProtocol] {
+        fatalError("need to be overrided.")
+    }
+}
